4f9227b0c3874f1bbcce638703aefcab5a02e8e3
onlytrleehieutrungg <trungtran2k01@gmail.com>
2022-10-01 17:58:57 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const SpotifyWebApi = require("spotify-web-api-node");
+const lyricsFinder = require("lyrics-finder");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    clientId: "8894f18801774d2b8cbfa00a6d5febb6",
+    clientSecret: "48dc64062afd4c4a82635a278d12fa4e",
+    redirectUri: "http://localhost:3000",
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(404);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    clientId: "8894f18801774d2b8cbfa00a6d5febb6",
+    clientSecret: "48dc64062afd4c4a82635a278d12fa4e",
+    redirectUri: "http://localhost:3000",
+  });
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(err => {
+      res.sendStatus(400);
+    });
+});
+
+app.get("/lyrics", async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) ||
+    "No Lyrics Found";
+  res.json({lyrics});
+});
+
+app.listen(3001);
